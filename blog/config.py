e58cad19dc14c35a6add86bdf36ca9fdef7b6d3c
ki-tsa <37465295+ki-tsa@users.noreply.github.com>
2020-07-31 11:06:56 +0545

    config

--- blog/config.py
+++ blog/config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.urandom(24).hex()
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///'+os.path.join(basedir, 'posts.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     confirm_deleted_rows = False
     MAIL_SERVER = 'smtp.googlemail.com'
