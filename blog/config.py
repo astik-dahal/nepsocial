6d48b05a70813c73b4d89fcd312d99a7e4a2270a
astik <37465295+astik-dahal@users.noreply.github.com>
2020-08-28 21:16:54 +0545

    Update config.py

--- blog/config.py
+++ blog/config.py
@@ -9,7 +9,7 @@ class Config:
     confirm_deleted_rows = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = '587'
-    MAIL_USE_TLS = '587'
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
     
